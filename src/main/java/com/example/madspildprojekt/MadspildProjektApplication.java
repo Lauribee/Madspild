3d2a7cadb504b0b87cd69944b55e0a7992e12c7f
Lauribee <laura_stroe_2001@yahoo.com>
2022-03-01 14:36:09 +0100

    oprettelse af projekt:)

---
+++ src/main/java/com/example/madspildprojekt/MadspildProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.madspildprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadspildProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadspildProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/madspildprojekt/MadspildProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madspildprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadspildProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
